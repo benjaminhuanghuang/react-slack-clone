96a83992b00c4086e0a37340c5c4743859c814f0
benjaminhuanghuang <bhuang@rms.com>
2020-08-11 20:48:57 -0700

    header css

--- slack-clone/src/Header.js
+++ slack-clone/src/Header.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Avatar } from "@material-ui/core";
 import AccessTimeIcon from  "@material-ui/icons/AccessTime"
+import SearchIcon from  "@material-ui/icons/Search"
+import HelpOutlineIcon from  "@material-ui/icons/HelpOutline"
+
 import "./Header.css";
 
 function Header() {
@@ -10,8 +13,13 @@ function Header() {
         <Avatar className="header__avatar" alt="Sonny SSS" src='' />
         <AccessTimeIcon/>
       </div>
-      <div className="header__search"></div>
-      <div className="header__right"></div>
+      <div className="header__search">
+        <SearchIcon/>
+        <input placeholder="search"/>
+      </div>
+      <div className="header__right">
+        <HelpOutlineIcon/>
+      </div>
     </div>
   );
 }
